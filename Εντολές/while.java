4418099f211bfd2de12250829c863d89565b33ef
Gr Code Club <135416047+GrCodeClub@users.noreply.github.com>
2023-06-08 16:50:41 +0300

    Create while.java

---
+++ Εντολές/while.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int i = 1; // Αρχική τιμή
+
+        // Επανάληψη όσο η τιμή του i είναι μικρότερη ή ίση με 5
+        while (i <= 5) {
+            System.out.println("Αριθμός: " + i);
+            i++; // Αύξηση της τιμής του i κατά 1
+        }
+    }
+}
