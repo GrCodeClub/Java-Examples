94fdd2487d5ec14e07dee0ca665e5425ad0e07c3
Gr Code Club <135416047+GrCodeClub@users.noreply.github.com>
2023-06-08 16:51:50 +0300

    Create if.java

---
+++ Εντολές/if.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int number = 5;
+
+        if (number < 0) {
+            System.out.println("Ο αριθμός είναι αρνητικός");
+        } else if (number == 0) {
+            System.out.println("Ο αριθμός είναι μηδέν");
+        } else {
+            System.out.println("Ο αριθμός είναι θετικός");
+        }
+    }
+}
