4c6ce06da0f18e47b4f1e70281903304537e713a
Gr Code Club <135416047+GrCodeClub@users.noreply.github.com>
2023-06-08 16:48:39 +0300

    Create for.java

---
+++ Εντολές/for.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // Επανάληψη από 1 έως 5
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Αριθμός: " + i);
+        }
+    }
+}
